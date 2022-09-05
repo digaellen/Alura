1274d94f8175bce61d4ce19bb6b8024bc49a4dcd
Ellen Camila <ellen.camila@outlook.com>
2022-09-05 16:10:34 -0300

    first commit

---
+++ Calculadora-de-Média/index.js
@@ -0,0 +1,27 @@
+var nome = "Ellen"
+var notaPrimeiroBimestre = 8
+var notaSegundoBimestre = 7
+var notaTerceiroBimestre = 4
+var notaQuartoBimestre = 2
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+console.log("Bem-vinda " + nome)
+console.log("Sua nota é " + notaFixada)
+
+
+/**
+
+Revisão aula 01:
+
+- Variáveis é onde o computador armazena os nossos comandos
+- Strings = palavras
+- console.log a gente consegue ver o que está escrito na tela
+- toFixed faz as operações matemáticas
+- As operações matemáticas seguem basicamente as mesmas regras 
+- Concatenação é a junção de uma string com uma variável
+
+
+Desafios:  
+
+**/
